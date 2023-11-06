5e76bae93a11eb9121fdc7a2681e11cc4a23ee4e
Tom Jankauski <tomjankauski@gmail.com>
2023-11-05 23:00:36 -0800

    add the generic flake to the templates

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   outputs = { self, ... }:
     {
       templates = {
+        flake = {
+          path = ./templates/flake;
+          description = ''Basic flake init.'';
+        };
         python = {
           path = ./templates/python;
           description = '''Basic python environment'';
